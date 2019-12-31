3f92e58abd9ee78d38a12aa7e572e37da8eb093f
kenzo <arwysyahputrasiregar@gmail.com>
2019-12-31 22:13:05 +0700

    first init

---
+++ src/Components/CheckOut.js
@@ -0,0 +1,156 @@
+import React from "react";
+import { Modal, Button } from "antd";
+import Axios from 'axios'
+import swal from "sweetalert";
+
+class CheckOut extends React.Component {
+  state = {
+    loading: false,
+    visible: false,
+    myData:[],
+    form:[]
+  };
+  formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = async() => {
+    let myData =[]
+
+    let dataCheckout = this.props.dataCheckout;
+    dataCheckout.map((data,index)=>{
+       myData.push(data.name)
+       
+      return(
+     console.log(data.name,'data.name'),
+     console.log('my data',myData)
+        
+      )
+
+    })
+   
+    let myDatan = myData.toString()
+    
+    console.log('ASDFGHJ',myDatan)
+    let total = this.props.total;
+    let ppn = total * 0.1;
+    let sumTotal = total + ppn
+    let quantity=this.props.quantities
+    let formData={
+      // total : total,
+      total:sumTotal,
+      product:myDatan,
+      quantities:quantity
+
+
+      
+
+      
+    }
+  
+    await Axios.post(`http://localhost:5050/history/`,formData).then(res=>{
+      console.log(res,'res')
+    }).then((swal("Transaction Succes!", "Thanks fror Using Our Services!", "success")))
+    
+    // console.log(dataCheckout[0].name,'checkout')
+    console.log(formData,'form')
+    // console.log(myData,'mydata')
+    // console.log('name',this.data.name)
+
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    let dataCheckout = this.props.dataCheckout;
+    let total = this.props.total;
+    let ppn = total * 0.1;
+    let sumTotal = total + ppn;
+    let quantities=this.props.quantities
+    // console.log("data", dataCheckout);
+    // console.log("total", total);
+    const { visible, loading } = this.state;
+    return (
+      <div>
+        <Button
+          type="primary"
+          style={{ left: -10, width: 240, backgroundColor: "#57CAD5" }}
+          onClick={() => this.showModal()}
+        >
+          Checkout
+        </Button>
+        <Modal
+          visible={visible}
+          title="Your Cart"
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button key="back" onClick={this.handleCancel}>
+              Return
+            </Button>,
+            <Button
+              key="submit"
+              type="primary"
+              loading={loading}
+              onClick={this.handleOk}
+            >
+              Submit
+            </Button>
+          ]}
+        >
+          <div style={{ marginLeft: -80 }}>
+            <table style={{ width: "100%" }}>
+              <tr style={{ textAlign: "center" }}>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+              {dataCheckout.map((items, i) => {
+                return (
+                  <tr style={{ textAlign: "center" }} key={i}>
+                    <td>{items.name}</td>
+                    <td> {this.formatNumber(items.quantity)}</td>
+                    <td>
+                      Rp. {this.formatNumber(items.price * items.quantity)}
+                    </td>
+                  </tr>
+                );
+              })}
+            </table>
+          </div>
+          <div style={{ marginTop: 20 }}>
+            <table style={{ width: "100%" }}>
+              <tr style={{ fontWeight: "bold" }}>
+                <td>PPN 10 % </td>
+                <td>:</td>
+                <td style ={{marginLeft:140}}>Rp. {this.formatNumber(ppn)}</td>
+              </tr>
+
+              <tr style={{ fontWeight: "bold" }}>
+                <td>Total</td>
+                <td>:</td>
+                <td>Rp. {this.formatNumber(sumTotal)}</td>
+                <td>Total Quantity</td>
+                <td>:</td>
+                <td> {this.formatNumber(quantities)}</td>
+              </tr>
+            </table>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default CheckOut;
---
+++ src/Components/Home.js
@@ -0,0 +1,306 @@
+import React from "react";
+
+import "antd/dist/antd.css";
+import { Layout, Menu, Icon, Row, Col,Card, Badge ,Button,Pagination} from "antd";
+import axios from 'axios'
+// import "../Components/styles/Home.css";
+import "../index.css";
+import AddProduct from '../Components/AddProduct'
+import Checkout from '../Components/CheckOut'
+const { Meta } = Card;
+const { Header, Sider, Content } = Layout;
+
+
+
+export default class Home extends React.Component {
+  state = {
+    collapsed: false,
+    items: [],
+    content:[],
+    minValue: 0,
+    maxValue: 9,
+    searchValue:'',
+   
+   
+    
+   
+  };
+  
+  async componentDidMount(){
+await this.handleProduct()
+
+
+  }
+handleProduct=()=>{
+  axios.get('http://localhost:5050/product').then(res=>{
+    this.setState({
+      content:res.data.response
+    })
+  })
+}
+
+
+onSearch=()=>{
+ 
+  axios.get(`http://localhost:5050/product//filter/product/search/:lemon`).then(res=>{
+    console.log(res)
+  })
+}
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
+  chooseItem = (item) => {
+    this.setState(prevState=>{
+      return{
+        items:[...prevState.items,item]
+      }
+    })
+  };
+  handleChange = value => {
+    if (value <= 1) {
+      this.setState({
+        minValue: 0,
+        maxValue: 9
+      });
+    } else {
+      this.setState({
+        minValue: this.state.maxValue,
+        maxValue: value * 9
+      });
+    }
+  };
+  handleMinus=(index)=>{
+    console.log('index')
+    const cartItem=this.state.items
+   const ca= cartItem[index].quantity --
+    this.setState({
+      items:cartItem
+    })
+    console.log('isi',ca)
+  }
+  handlePlus=(index)=>{
+    const cartItem = this.state.items
+    cartItem[index].quantity ++
+  this.setState( {items:cartItem})
+  }
+  formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
+  onchangeSearch = event => {
+    const search =event.target.value  
+
+    this.setState({
+      searchValue: search
+    })
+
+  }
+  removeItem(id){
+    this.setState({
+      items: this.state.items.filter(item => item.id_product !== id)
+    });
+
+  }
+
+  handleSearch = event =>{
+    event.preventDefault()
+
+    axios.get(`http://localhost:5050/product//filter/product/search/${this.state.searchValue}`).then(res=>
+   
+    
+    this.setState({
+      content:res.data.response
+    })
+    // console.log(res.data.name)
+    // this.setState({
+    //   searchPoint:res.data.response
+    // })
+    )
+
+  }
+  render() {
+    const { content,items } = this.state;
+    let cartItem = this.state.items
+    let total = cartItem.reduce((prev, next) => prev + next.quantity * next.price, 0);
+    let quantities=cartItem.reduce((prev,next)=>prev+next.quantity,0)
+    console.log(total,'total')
+    console.log(this.state.content);
+    console.log(quantities,'quantities')
+    return (
+      <Row>
+        <Col span={18}>
+          <Layout>
+            <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+              <div className="logo" />
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+                <Menu.Item key="1">
+                  <Icon type="shop" style={{ fontSize: 29 }} />
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Icon type="video-camera" style={{ fontSize: 29 }} />
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <Icon
+                    type="plus-square"
+                    style={{ color: "green", fontSize: 29, left: 4 }}
+                  />
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Layout>
+              <AddProduct />
+              <Header style={{ background: "#fff", padding: 0 }}>
+                <Row>
+                  <Col span={8}>
+                    {" "}
+                    <Icon
+                      className="trigger"
+                      type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
+                      onClick={this.toggle}
+                      style={{ fontSize: 29,marginLeft:20 }}
+                    />
+                  </Col>
+                  <Col span={8}>
+                    {" "}
+                    <div>
+                    <form onSubmit={this.handleSearch}>
+          <div className="input-field" style ={{marginLeft:-40,width:"30%" ,height:30}}>
+              {/* <Icon type="search" /> */}
+          <input id="search" type="search" required onChange={this.onchangeSearch} style={{color:"black" ,marginLeft:80,height:40,backgroundColor:"white"}}/>
+               
+        
+               
+                  </div>
+          </form>
+                    </div>
+                  </Col>
+                </Row>
+              </Header>
+              <Content
+                style={{
+                  margin: "24px 16px",
+                  padding: 24,
+                  background: "#fff",
+                  minHeight: "100vh"
+                }}
+              >
+                <Row>
+                {
+          content.length > 0 &&
+         
+                  content.slice(this.state.minValue,this.state.maxValue).map((cont, index) => {
+                    return (
+                      <Col span={8} key={index}>
+                        <Card
+                          style={{ height: '30%', width: '22,5%', marginTop: 20 ,borderRadius:10}}
+                          hoverable
+                          onClick={()=> !items.filter(cart => cart.id_product === cont.id_product).length > 0 && this.chooseItem({...cont, quantity:1})}
+                          cover={
+                            <img
+                              alt="example"
+                              src={cont.image_url}
+                              style={{ height: '30%', width: '22,5%',borderRadius:10}}
+                            />
+                          }
+                        >
+                          <Meta title={cont.name} />
+                          <p>Rp. {this.formatNumber(cont.price)} </p>
+                        </Card>
+                      </Col>
+                    );
+                  })
+                  }
+                </Row>
+              
+              </Content>
+              <div style={{marginLeft:330,marginTop:-20}}>
+              <Pagination 
+          defaultCurrent={1}
+          defaultPageSize={9}
+          onChange={this.handleChange}
+          total={15}
+        />
+        </div>
+            </Layout>
+           
+          </Layout>
+          
+        </Col>
+
+        <Col style={{ border: "1px solid black", height: 45 }} span={6}>
+          <p style={{ textAlign: "center" }}>
+            Cart
+            <Badge count={this.state.count} style={{ backgroundColor: "blue",color:'black' }} />
+          </p>
+        </Col>
+        <Col span={6}>
+          
+          <Row>
+         {this.state.items.length > 0 ? (
+            items.map((c,i)=>(
+              <div style={{ top: 30 }} 
+              key={i}>
+            <Col span={9}>
+              <img
+                alt="example"
+                src={c.image_url}
+                style={{ height: '60,5%', width: '88%' ,borderRadius:5}}
+              />
+              
+            </Col>
+            <Col span={10}>
+              <p style={{left:100,fontWeight:'bold'}}>{c.name}</p>
+              <Button  className="button" style={{backgroundColor:'#aff589',color:'black'}}
+             onClick={()=>{this.handleMinus(i)}} >
+              <Icon type="minus" />
+              </Button>
+  <input type="text" style={{width:40,height:30,textAlign:'center'}} value={c.quantity}></input>
+            <Button type="primary" style={{backgroundColor:'#aff589',color:'black'}}
+            onClick={()=>{this.handlePlus(i)}}>
+            <Icon type="plus" />
+            </Button>
+<p style={{height:40}}>Total :  {c.price*c.quantity}</p>
+            </Col>
+            <Col span={5} style={{marginTop:20}}>
+             {c.quantity===0 ? this.removeItem(c.id_product):""}
+             <p
+               onClick={()=>this.removeItem(c.id_product)}><a>remove</a></p>
+              <p>
+
+                Rp. {this.formatNumber(c.price)}
+              </p>
+            </Col>
+           
+            </div>
+            ))
+            ): (
+
+              <div style={{ paddingTop: "40%" }}>
+                <img
+                  width="100%"
+                  src="https://www.razencustoms.com/includes/img/empty-cart.png"
+                  alt="cart"
+                />
+              </div>
+              )}
+          </Row>
+           
+          <div className="check" style={{marginLeft:30,marginTop:120}} >
+          <p style={{fontWeight:'bold'}}>Total Belanja : Rp.{this.formatNumber(total)}</p>
+          {/* <Button type="primary" style={{width:280,backgroundColor:'#57CAD5'}}>Checkout</Button> */}
+          <Checkout
+          quantities={quantities}
+          total={total}
+          dataCheckout={this.state.items} />
+        <p></p>
+          <Button type="primary" style={{left:-10,width:240,backgroundColor:'#F24F8A'}}>Cancel</Button>
+          </div>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+// ReactDOM.render(<SiderDemo />, mountNode);
